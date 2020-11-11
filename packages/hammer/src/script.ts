e8eaac3ec024a5763c5ab3d3e3ea7851c5737c8b
KovacZan <zan@protokol.com>
2020-11-11 11:20:03 +0100

    feat(hammer): marvel collection

--- packages/hammer/src/script.ts
+++ packages/hammer/src/script.ts
@@ -1,4 +1,7 @@
 import fifaCollection = require("./data/collections/fifa-collection.json");
+import marvelCollection = require("./data/collections/marvel-collection.json");
+import ironManAsset = require("./data/assets/marvel/iron-man.json");
+
 
 import delay from "delay";
 import faker from "faker";
@@ -9,38 +12,64 @@ import { setupScript } from "./setup";
 export const main = async () => {
 	setupScript();
 
-	const scriptType = new FillScript();
+	// const scriptType = new FillScript();
+	//
+	// console.log("Create collections");
+	// await scriptType.createCollections(1, 5, fifaCollection);
+	//
+	// await delay(8000);
+	//
+	// console.log("Create assets");
+	// await scriptType.createAssets(1, 5, 3, {
+	// 	attributes: {
+	// 		name: faker.name.findName(),
+	// 		pac: faker.random.number({ max: 100, min: 1 }),
+	// 		sho: faker.random.number({ max: 100, min: 1 }),
+	// 		pas: faker.random.number({ max: 100, min: 1 }),
+	// 		dri: faker.random.number({ max: 100, min: 1 }),
+	// 		def: faker.random.number({ max: 100, min: 1 }),
+	// 		phy: faker.random.number({ max: 100, min: 1 }),
+	// 	},
+	// });
+	//
+	// await delay(8000);
+	//
+	// console.log("Create auctions");
+	// await scriptType.createAuctions(2, 4);
+	//
+	// await delay(8000);
+	//
+	// console.log("Create bids");
+	// await scriptType.createBids(10, 2);
+	//
+	// await delay(8000);
+	//
+	// console.log("Create trades");
+	// await scriptType.createTrades();
 
-	console.log("Create collections");
-	await scriptType.createCollections(1, 5, fifaCollection);
+	const fillScriptMarvelCollection = new FillScript();
 
+	console.log("Create marvel collections");
+	await fillScriptMarvelCollection.createCollections(1, 5, marvelCollection);
 	await delay(8000);
 
-	console.log("Create assets");
-	await scriptType.createAssets(1, 5, 3, {
-		attributes: {
-			name: faker.name.findName(),
-			pac: faker.random.number({ max: 100, min: 1 }),
-			sho: faker.random.number({ max: 100, min: 1 }),
-			pas: faker.random.number({ max: 100, min: 1 }),
-			dri: faker.random.number({ max: 100, min: 1 }),
-			def: faker.random.number({ max: 100, min: 1 }),
-			phy: faker.random.number({ max: 100, min: 1 }),
-		},
+	console.log("Create Iron Man assets");
+	await fillScriptMarvelCollection.createAssets(1, 5, 3, {
+		attributes: ironManAsset,
 	});
 
 	await delay(8000);
 
 	console.log("Create auctions");
-	await scriptType.createAuctions(2, 4);
+	await fillScriptMarvelCollection.createAuctions(2, 4);
 
 	await delay(8000);
 
 	console.log("Create bids");
-	await scriptType.createBids(10, 2);
+	await fillScriptMarvelCollection.createBids(10, 2);
 
 	await delay(8000);
 
 	console.log("Create trades");
-	await scriptType.createTrades();
+	await fillScriptMarvelCollection.createTrades();
 };
