e52417c8b7f4bc56409c3dfebd766aa50753080a
KovacZan <zan@protokol.com>
2021-02-19 17:38:26 +0100

    feat(sets): export in root

---
+++ packages/sets/src/index.ts
@@ -0,0 +1,9 @@
+import * as AREX from "./arex";
+import * as Breitling from "./breitling";
+import * as Fifa from "./fifa";
+import * as Iwc from "./iwc";
+import * as Marvel from "./marvel";
+import * as NascarHeroCard from "./nascar-hero-card";
+import * as NascarTeam from "./nascar-team";
+
+export default { AREX, Breitling, Fifa, Iwc, Marvel, NascarHeroCard, NascarTeam };
