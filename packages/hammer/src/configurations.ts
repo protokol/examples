ed8c61106f740631b7a16dc9afb1116ef8e83a27
KovacZan <zan@protokol.com>
2021-07-21 04:18:36 +0200

    fix(hammer): change url and lower the split amount

--- packages/hammer/src/configurations.ts
+++ packages/hammer/src/configurations.ts
@@ -1,7 +1,7 @@
 import passphrases = require("./data/networks/delegates.json");
 
 export const configurations = {
-	clientHost: "https://proto-devnet.protokol.sh/api",
+	clientHost: "https://devnet-explorer.protokol.sh/api",
 	passphrasesFile: passphrases,
 	delayTime: 8000,
 };
